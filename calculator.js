a809725f8472b1c9d4e21707417b205a181efff9
JoseBrouwer <josebrouwer1@gmail.com>
2024-12-06 11:51:28 -0500

    operate(operator, a, b) for on the fly operation

--- calculator.js
+++ calculator.js
@@ -1,3 +1,20 @@
+function operate(operator, a, b) {
+    console.log(operator);
+    if(operator !== "+" && operator !== "-" && operator !== "*" && operator !== "/")
+    {
+        console.log("Not a valid operator. Operation will not complete.");
+        return NaN;
+    }
+    else if(operator === "+")
+        return add(a, b);
+    else if(operator === "-")
+        return subtract(a, b);
+    else if(operator === "*")
+        return multiply(a, b);
+    else if(operator === "/")
+        return divide(a,b);
+}
+
 function add(a, b) {
      return a + b;
 };
@@ -19,10 +36,13 @@ function divide(a, b) {
     return a / b;
 };
 
+let operation = prompt("Enter the operation you would like to perform (+, -, *, /): ")
 let a = parseFloat(prompt("Enter the first value of the operation: "));
 let b = parseFloat(prompt("Enter the second value of the operation: "));
+let result = operate(operation, a , b);
+console.log(result);
 
-console.log(`Addition: ${add(a, b)}`);
-console.log(`Subtraction: ${subtract(a, b)}`);
-console.log(`Multiplication: ${multiply(a, b)}`);
-console.log(`Division: ${divide(a, b)}`);
+// console.log(`Addition: ${add(a, b)}`);
+// console.log(`Subtraction: ${subtract(a, b)}`);
+// console.log(`Multiplication: ${multiply(a, b)}`);
+// console.log(`Division: ${divide(a, b)}`);
