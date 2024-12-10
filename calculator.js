93f41f81dd8aa14a0ed607c5cfc412296f2908c8
JoseBrouwer <josebrouwer1@gmail.com>
2024-12-10 15:38:02 -0500

    Expressions with more than one operation have ops sorted on PEMDAS

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 let currentOp = "", resultString = "", operator = "";
 let a, b, resultNumber;
+const PEMDAS = ['(', ')', '**', '*', '/', '+', '-'];
 
 function init(){
     const buttons = document.querySelectorAll(".flex-btn");
@@ -24,14 +25,29 @@ function display(button) {
     }
     else if(button.classList.contains("equals")) //if we click equals
     {
+        //modify to evaluate longer expressions and use PEMDAS
+        //check size of split, if its longer than three (always odd by taking a = result?)
+        //enforce PEMDAS from left to right by grouping operations
         let parts = currentOp.split(" ");
-        a = parseFloat(parts[0]);
-        operator = parts[1];
-        b = parseFloat(parts[2]);
-        resultNumber = operate(operator, a, b);
-        resultString = String(resultNumber)
-        currentOp = resultString;
-        display.textContent = resultString;
+        if(parts.length > 3)
+        {
+            //return an array that contains all the operators in the expression
+            let operatorList = parts.filter(operator => PEMDAS.includes(operator));
+            console.log(`Before Sort: ${operatorList}`);
+            operatorList.sort(oop); //sort the array to use PEMDAS
+            console.log(`After Sort: ${operatorList}`);
+  
+        }
+        else
+        {
+            a = parseFloat(parts[0]);
+            operator = parts[1];
+            b = parseFloat(parts[2]);
+            resultNumber = operate(operator, a, b);
+            resultString = String(resultNumber)
+            currentOp = resultString;
+            display.textContent = resultString;
+        }
     }
     else if(button.classList.contains("clear"))
     {
@@ -40,6 +56,18 @@ function display(button) {
     }
     
 }
+//sort based on the index of PEMDAS elements
+function oop(a, b){
+    let aIndex = PEMDAS.indexOf(a);
+    let bIndex = PEMDAS.indexOf(b);
+    if(aIndex > bIndex) //a is further down the list, less priority
+        return 1;
+    if(aIndex == bIndex) //dont move
+        return 0;
+    if(aIndex < bIndex) //a is closer to the start than b, more priority
+        return -1;
+}
+
 function operate(operator, a, b) {
     console.log(operator);
     if(operator !== "+" && operator !== "-" && operator !== "*" && operator !== "/")
